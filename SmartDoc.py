5ea3df31dc6cd7d7973a80f1c48930d2a2ed8453
ZXXSC <49486351+ZXXSC@users.noreply.github.com>
2019-04-24 23:34:08 +0800

    Update SmartDoc.py

--- SmartDoc.py
+++ SmartDoc.py
@@ -2,23 +2,29 @@ def lines(file):
     for line in file:yield line
     yield '\n'
 
-def blocks(file):
-    block = []
-    for line in lines(file):
-        if line.strip():
-           block.append(' '+line+'<br>')
-        elif block:
-           yield ''.join(block).strip()
-           block=[]
 import sys,re
-
 
 print ('<html><body>')
 
-for block in blocks(sys.stdin):
-    block = re.sub(r'\*(.+?)\*',r'<em>\1</em>',block)
-    print('<p>')
-    print(block)
-    print('</p>')
+for line in lines(sys.stdin):
+    line = re.sub(r'\*(.+?)\*',r'<em>\1</em>',line)
+    if line.startswith('@Requirement [id=rq1]'):
+        print('<pre><a href="Word_Frequency.html#def1" name="rq1" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('@Requirement [id=rq2]'):
+        print('<pre><a href="Word_Frequency.html#def2" name="rq2" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('@Requirement [id=rq3]'):
+        print('<pre><a href="Word_Frequency.html#def3" name="rq3" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('@Requirement [id=rq4]'):
+        print('<pre><a href="Word_Frequency.html#def4" name="rq4" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('#{see rq1}'):
+        print('<pre><a href="SmartDocSRS.html#rq1" name="def1" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('#{see rq2}'):
+        print('<pre><a href="SmartDocSRS.html#rq2" name="def2" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('#{see rq3}'):
+        print('<pre><a href="SmartDocSRS.html#rq3" name="def3" target="_blank">'+line+'</a></pre>')
+    elif line.startswith('#{see rq4}'):
+        print('<pre><a href="SmartDocSRS.html#rq4" name="def4" target="_blank">'+line+'</a></pre>')
+    else:
+        print('<pre>'+line+'</pre>')
 
 print('</body></html>')
