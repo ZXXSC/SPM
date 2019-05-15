db4f1c969738f71a1d7cc88a6ff3b9f395a6b50d
ZXXSC <49486351+ZXXSC@users.noreply.github.com>
2019-05-16 00:36:22 +0800

    Update SmartDoc.py
    改变html文件生成方式，使得生成的html文件名不用用户自取，实现两个文件一起输入

--- SmartDoc.py
+++ SmartDoc.py
@@ -1,33 +1,14 @@
-def lines(file):
-    for line in file:yield line
-    yield '\n'
-
+import webbrowser
 import sys,re
+from sys import argv
 
-print ('<html><body>')
+script,srs_file,code_file=argv
+file1=open(srs_file,"r")
+file2=open(code_file,"r")
+GEN_HTML1 = "srs.html" 
+GEN_HTML2 = "code.html"
+f1 = open(GEN_HTML1,'w')
+f2 = open(GEN_HTML2,'w')
 
-for line in lines(sys.stdin):
-    line = re.sub(r'\*(.+?)\*',r'<em>\1</em>',line)
-    if line.startswith('@Requirement [id=rq1]'):
-        print('<pre><a href="Word_Frequency.html#def1" name="rq1" target="_blank">'+line+'</a></pre>')
-        print('<a href="Word_Frequency.html#def11" name="rq1" target="_blank">[id=rq11]</a>')
-    elif line.startswith('@Requirement [id=rq2]'):
-        print('<pre><a href="Word_Frequency.html#def2" name="rq2" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('@Requirement [id=rq3]'):
-        print('<pre><a href="Word_Frequency.html#def3" name="rq3" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('@Requirement [id=rq4]'):
-        print('<pre><a href="Word_Frequency.html#def4" name="rq4" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('#{see rq1}'):
-        print('<pre><a href="SmartDocSRS.html#rq1" name="def1" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('    #{see rq1}'):
-        print('<pre><a href="SmartDocSRS.html#rq1" name="def11" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('#{see rq2}'):
-        print('<pre><a href="SmartDocSRS.html#rq2" name="def2" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('#{see rq3}'):
-        print('<pre><a href="SmartDocSRS.html#rq3" name="def3" target="_blank">'+line+'</a></pre>')
-    elif line.startswith('#{see rq4}'):
-        print('<pre><a href="SmartDocSRS.html#rq4" name="def4" target="_blank">'+line+'</a></pre>')
-    else:
-        print('<pre>'+line+'</pre>')
-
-print('</body></html>')
+webbrowser.open(GEN_HTML1,new = 1) 
+webbrowser.open(GEN_HTML2,new = 1) 
