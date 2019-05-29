da4aa02bc80f4139e1bc4ea5d683d3dd254e9614
ZXXSC <49486351+ZXXSC@users.noreply.github.com>
2019-05-30 03:47:47 +0800

    Update SmartDoc.py
    改变循环方式

--- SmartDoc.py
+++ SmartDoc.py
@@ -32,26 +32,53 @@ for line in file1:
 
 
 
-j=1
-file2=open(code_file,"r")
+result=[]
+d={}
 for line in file2:
-    m=str(j)
-    str1="#{see rq"+m+"}"
-    if str1 in line:
-        message = """
-        <pre><a href="srs.html#rq%d" name="rq%d">%s</a></pre>
-        """%(j,j,line)
-        j+=1
-    elif "#{see rq1}" in line:
-        if not line.startswith("#"):
-	        message = """
-            <pre><a href="srs.html#rq%d" name="rq11">%s</a></pre>
-           """%(n,line)
-    else:
-        message = """
-        <pre>%s</pre>
-        """%(line)
-    f2.write(message) 
+    if line.find("see rq")>=0:
+        pattern = re.compile(r'rq\d+')
+        rq=pattern.findall(line)
+        result.append(rq[0])
+        if rq[0] in result:
+            if not rq[0] in d:
+                d[rq[0]]=1
+            else:
+                d[rq[0]]+=1
+
+key=list(d.keys())
+value=list(d.values())
+i=0
+
+for j in range(-1,len(d)-1):
+    j+=1
+    file2=open(code_file,"r")#待修改
+    f2.write("<html><head></head><body>")
+    for line in file2:    	
+        if (line.find("see "+key[j])>0):
+            print(j)
+            if value[j]>1:
+                i+=1
+                message="""
+                <pre><a href="srs.html#%s" name="%s%d">%s</a></pre>
+                """%(key[j],key[j],i,line)
+                message2="""
+                <pre><a href="code.html#%s%d">%s%d</a></pre>
+                """%(key[j],i,key[j],i)
+                f1.write(message2)
+                f2.write(message)
+            print(value[j]==1)
+            if value[j] is 1:
+                print(value[j])
+                message = """
+                <pre><a href="srs.html#%s" name="%s">%s</a></pre>
+                """%(key[j],key[j],line)
+                f2.write(message)
+            print(j)
+        else:
+            message = """
+            <pre>%s</pre>
+            """%(line)
+            f2.write(message)
 
 f1.write("</body></html>")
 f2.write("</body></html>")
